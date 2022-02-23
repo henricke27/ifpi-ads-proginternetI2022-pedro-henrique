5836cb42533e19fdb46a88af20fc2cf349c7ed76
henricke27 <84256973+henricke27@users.noreply.github.com>
2022-02-22 23:09:37 -0300

    Add files via upload

---
+++ exercicio_01/q2.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+const axios = require('axios').default
+
+function infoResponse(){
+
+    let url = prompt("URL: ")
+
+    if(!url.includes("https://")){
+        url = "https://" + url;
+    }
+
+    axios.get(url)
+        .then(function(response){
+            //sucesso na execução;
+            console.log(`Status code: ${response.status} - ${response.statusText}`)
+            console.log(`Content type: ${response.headers['content-type']}`)
+            console.log(`Date: ${response.headers['date']}`)
+            console.log(`Etag: ${response.headers['etag']}`)
+            console.log(`Vary: ${response.headers['vary']}`)
+        })
+        .catch(function(error){
+            //falha na execução
+            console.error(error)
+        })
+}
+
+infoResponse()
---
+++ exercicio_01/q3.js
@@ -0,0 +1,24 @@
+const fs = require('fs')  
+const axios = require('axios').default
+const prompt = require('prompt-sync')();
+
+function downloadImage(){
+
+    let url = prompt("URL da imagem: ")
+
+    if(!url.includes("https://")){
+        url = "https://" + url;
+    }
+
+    axios({
+        method: 'GET',
+        url: url,
+        responseType: 'stream',
+    }).then(function(response){
+        response.data.pipe(fs.createWriteStream("image-downloaded.jpg"))
+    }).catch(function(error){
+        console.error(error)
+    }) 
+}
+
+downloadImage();
