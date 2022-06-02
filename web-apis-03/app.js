4f8c0785db009dda16c841311b078a32d556284b
henricke27 <84256973+henricke27@users.noreply.github.com>
2022-06-02 11:16:45 -0300

    Add files via upload

---
+++ web-apis-03/app.js
@@ -0,0 +1,160 @@
+import {Post} from "./Post.js"
+
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
+import serviceAccount from './rede-social-1920e-firebase-adminsdk-j0px1-995200d250.json' assert {type: "json"};
+import express, { json, request, response, text, urlencoded } from 'express';
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+const db = getFirestore()
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+
+app.listen(3000, () => console.log("run"));
+
+
+app.get("/posts", async (request, response) => { 
+    let page = request.query.page
+
+    let code = 200;
+    let docs = await db.collection('posts').orderBy('data').get();
+    let posts = new Array()
+
+    if(page){
+        page--;
+        let limit = 2; 
+        let firstDoc;
+
+        try {
+            firstDoc = docs.docs[page * limit]
+            docs = await db.collection('posts').orderBy('data').startAt(firstDoc).limit(limit).get()
+        } catch (error) {
+            code = 204;
+            posts = [];
+        }
+    }
+    docs.forEach(post => posts.push(post.data()));
+    response.status(code).json(posts);
+
+})
+
+app.get("/posts/:id", async (request, response) => { 
+    let id = request.params.id
+    let postFounded = db.collection('posts').doc(id);
+
+    let doc = await postFounded.get()
+
+    let post;
+    let code = 200
+
+    if(!doc.exists){
+        code = 404
+    }else{
+        post = doc.data()
+    }
+
+    response.status(code).json(post)
+})
+
+app.get("/post", async (request, response) => { 
+    let id = request.query.q
+    let postFounded = db.collection('posts').doc(id);
+
+    let doc = await postFounded.get()
+
+    let post;
+    let code = 200
+
+    if(!doc.exists){
+        code = 404
+    }else{
+        post = doc.data()
+    }
+
+    response.status(code).json(post)
+})
+
+app.delete("/posts/:id", async (request, response) => { 
+    let id = request.params.id
+    let postFounded = db.collection('posts').doc(id);
+
+    let doc = await postFounded.get()
+
+    let code = 204
+
+    if(!doc.exists){
+        code = 404
+    }else{
+        postFounded.delete();
+    }
+
+    response.status(code).send()
+})
+
+
+app.post("/posts", async (request, response) => { 
+    let post = new Post(request.body.text);
+
+    await db.collection('posts').add(JSON.parse(JSON.stringify(post)))
+
+    response.status(201).json();
+})
+
+
+app.put("/posts/:id", async (request, response) => { 
+    let id = request.params.id
+
+    let postFounded = db.collection('posts').doc(id);
+    let doc = await postFounded.get()
+
+    let post;
+    let code = 200
+
+    if(!doc.exists){
+        code = 404
+    }else{
+        post = request.body
+        await db.collection('posts').doc(id).set(post)
+    }
+    response.status(code).send();
+})
+
+app.patch("/posts/:id", async (request, response) => { 
+    let id = request.params.id
+
+    let postFounded = db.collection('posts').doc(id);
+    let doc = await postFounded.get()
+
+    let post;
+    let code = 200
+
+    if(!doc.exists){
+        code = 404
+    }else{
+        post = request.body
+        await db.collection('posts').doc(id).update(post)
+    }
+    response.status(code).send();
+})
+
+app.patch("/posts/:id/like", async (request, response) => { 
+    let id = request.params.id
+
+    let postFounded = db.collection('posts').doc(id);
+    let doc = await postFounded.get()
+
+    let code = 200
+
+    if(!doc.exists){
+        code = 404
+    }else{
+        await db.collection('posts').doc(id).update({likes: doc.data().likes + 1})
+    }
+    response.status(code).send();
+})
